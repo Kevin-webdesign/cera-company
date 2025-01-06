a7d1f4c5ff209d332ae9a25733102fc3ad3a7b96
Kevin-webdesign <uzamurerakevin01@gmail.com>
2025-01-06 11:36:20 +0200

    add all

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -25,11 +25,11 @@ const Footer = () => {
         <div className="footer-section">
           <h4>Follow Us</h4>
           <div className="social-media">
+          <a href="https://x.com/cerachancellor"><img src={logo} alt="" /></a>
             <a href="https://www.instagram.com/rwanyagasorechancelier?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
               <i><FaInstagram/></i>
             </a>
             <a href="https://www.linkedin.com/in/chancelier-cera-rwanyagasore-6841a2128?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "><FaLinkedin /></a>
-            <a href="https://x.com/cerachancellor"><img src={logo} alt="" /></a>
           </div>
         </div>
       </div>
